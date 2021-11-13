7b7bcca8789f7177c104df88f45ddec823500613
Paata Kurdadze <paata2016@gmail.com>
2021-11-14 00:43:41 +0400

    end json and start ajax

--- Json/App_Start/RouteConfig.cs
+++ Json/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Json
@@ -12,12 +8,23 @@ namespace Json
     public static void RegisterRoutes(RouteCollection routes)
     {
       routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+      // RouteConfig -ში name დავამატეთ
+      // თუ RouteConfig-ში არ შევცვლიდით მაშინ ასე შეგვეძლო არგუმენტების გადაცემა /Home/index/?id=1&name=Paata
       routes.MapRoute(
           name: "Default",
-          url: "{controller}/{action}/{id}",
-          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+          url: "{controller}/{action}/{id}/{name}",
+          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, name = UrlParameter.Optional }
       );
+
+
+      //შეიძლება სხვა როუტინგიც დავწეროთ პარალელურად
+      routes.MapRoute(
+         name: "Default2",
+         url: "{controller}/{action}/{id}/{name}",
+         defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, name = "Paata" }
+     );
     }
+
+
   }
 }
--- Json/Controllers/HomeController.cs
+++ Json/Controllers/HomeController.cs
@@ -1,30 +1,44 @@
-﻿using System;
+﻿using Json.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Json.Controllers
 {
   public class HomeController : Controller
   {
-    public ActionResult Index()
-    {
-      return View();
-    }
-
-    public ActionResult About()
-    {
-      ViewBag.Message = "Your application description page.";
+    public ActionResult Index(int id, string name)     // https://localhost:44389/Home/index/1 an https://localhost:44389/Home/index/1/Paata
+    {                                                  // RouteConfig -ში name დავამატეთ
+      ViewBag.id = id;                                 // თუ RouteConfig-ში არ შევცვლიდით მაშინ ასე შეგვეძლო არგუმენტების გადაცემა /Home/index/?id=1&name=Paata
+      ViewBag.name = name;
 
       return View();
     }
 
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Your contact page.";
 
-      return View();
+    public JsonResult GetDat()         //  https://localhost:44389/Home/Getdat
+    {
+      MyDbDataContext db = new MyDbDataContext();
+      List<User> userList = new List<User>();
+      userList = db.Users.Where(x => x.Id > -1).ToList();
+
+      return Json(userList, JsonRequestBehavior.AllowGet);
     }
+
+
+
+    //public ActionResult About()
+    //{
+    //  ViewBag.Message = "Your application description page.";
+
+    //  return View();
+    //}
+
+    //public ActionResult Contact()
+    //{
+    //  ViewBag.Message = "Your contact page.";
+
+    //  return View();
+    //}
   }
 }
