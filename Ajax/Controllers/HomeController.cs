ce5a83c5e59295d84c07c74eb865f93b8d99eecc
Paata Kurdadze <paata2016@gmail.com>
2021-11-14 02:00:57 +0400

    Jquery მონაცემები  Ajax მეშვეობით გავაგზავნეთ Json ფორმატში

---
+++ Ajax/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Ajax.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Ajax.Controllers
+{
+  public class HomeController : Controller
+  {
+    // GET: Home
+    public ActionResult Index()
+    {
+      MydbDataContext db = new MydbDataContext();
+
+      return View(db.students.ToList());
+    }
+
+    [HttpPost]
+    public JsonResult Index(string name, string surname, string email, int age)
+    {
+      student s = new student()
+      {
+        Name = name,
+        Surname = surname,
+        Email = email,
+        Age = age
+      };
+
+      MydbDataContext db = new MydbDataContext();
+      db.students.InsertOnSubmit(s);
+      db.SubmitChanges();
+
+
+      return Json(true, JsonRequestBehavior.AllowGet);
+    }
+
+  }
+}
