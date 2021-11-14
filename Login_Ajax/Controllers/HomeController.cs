560a779dc07c4660ed5fe1c2e3e28cf209fcb95f
Paata Kurdadze <paata2016@gmail.com>
2021-11-14 23:25:41 +0400

    add project Login_Ajax

---
+++ Login_Ajax/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Login_Ajax.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Login_Ajax.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public JsonResult Index(string name, string surname)
+    {
+      MyDbDataContext db = new MyDbDataContext();
+      student s = db.students.Where(x => x.Name == name && x.Surname == surname).FirstOrDefault();
+      if (s == null)
+      {
+        return Json("fail", JsonRequestBehavior.AllowGet);
+      }
+      else
+      {
+        return Json(true, JsonRequestBehavior.AllowGet);
+      }
+    }
+
+
+    public ActionResult About()
+    {
+      return View();
+    }
+  }
+}
