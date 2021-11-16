da0d45bc02dfca556d2a7556a81a0145cb3efb17
Paata Kurdadze <paata2016@gmail.com>
2021-11-16 21:09:32 +0400

    Add project Comments

---
+++ Comments/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Comments.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Comments.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      MydbDataContext db = new MydbDataContext();
+
+      return View(db.comments.ToList().OrderByDescending(x => x.ID));
+    }
+
+    public JsonResult Add(string text)
+    {
+      MydbDataContext db = new MydbDataContext();
+
+      comment c = new comment();
+      c.Comment1 = text;
+
+      db.comments.InsertOnSubmit(c);
+      db.SubmitChanges();
+
+      return Json(true, JsonRequestBehavior.AllowGet);
+    }
+  }
+}
