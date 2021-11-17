13f62729a56f386a8909ce715d1930e409f9804c
Paata Kurdadze <paata2016@gmail.com>
2021-11-17 21:17:52 +0400

    Add Basket 1

---
+++ Basket/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Basket.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace Basket.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      MydbDataContext mydbDataContext = new MydbDataContext();
+
+      return View(mydbDataContext.product_news.ToList());
+    }
+
+    [HttpPost]
+    public JsonResult Index(int id)
+    {
+
+
+      return Json();
+    }
+  }
+}
